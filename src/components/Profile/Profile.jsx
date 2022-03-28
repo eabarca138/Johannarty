0163b23f7f08e5796ba80537076b543f77c30736
Esteban Abarca <eabarca171@gmail.com>
2022-03-28 16:27:26 -0300

    profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,36 @@
+import './Profile.css'
+import foto from '../../img/foto.jpg'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <>
+      <section className="profile">
+        <h2>Johann Arty</h2>
+        <img src={foto} alt="johann arty" className='profile-image'/>
+      </section>
+
+      <section className='description'>
+      <p>
+        His music has been supported by artists such as Abstract Division,
+        Adriana Lopez, Ancient Methods, Antonio De Angelis, A Thousand Details,
+        Axel Karakasis, Bernardo Hangar, Chris Liebing, Dave Clarke, David
+        Leese, Dax J, Drumcell, Drop-E, DVS1, Fixeer, Fixon, HD Substance, Jörg
+        Rodriguez, Kaiser, Marco Bailey, Michel Lauriola, Milo Raad, Oxygeno,
+        Ricardo Garduno, Richie Hawtin, Sam Paganini, Slam, Svarog, Takaaki
+        Itoh, Unbalance, Unkle Fon, Vegim and more.
+      </p>
+      <p>
+        Being a member of the Insight agency roster allowed him to perform on
+        different underground parties from Buenos Aires, including the
+        recognized techno club Under Club.
+      </p>
+      <p>
+        His sets focus on the evolving textures and atmospheres that each track
+        evoques, incorporating a wide array of sounds that develop with weight
+        and groove on the dancefloor.
+      </p>
+      </section>
+    </>
   )
 }
 
